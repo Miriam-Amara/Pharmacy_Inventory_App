5f7c8e4e81ae9946bd7e641ebca4d890f9c98119
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-26 04:26:29 +0100

    feat: add product page display logic

---
+++ frontend/src/hook/ProductLogic.js
@@ -0,0 +1,185 @@
+/* */
+
+import { useState, useEffect } from "react";
+
+import { showToast } from "../utils/toast";
+import { productValidationSchema } from "../utils/formInputValidation";
+import { fetchAllBrandsApi } from "../api/brands";
+import { fetchAllCategoriesApi } from "../api/categories";
+import {
+  addProductApi,
+  fetchProductApi,
+  fetchAllProductsApi,
+  updateProductApi,
+  fetchFilteredProductsByBrandAndCategory,
+  deleteProductApi,
+} from "../api/products";
+
+
+
+export default function useProductLogic() {
+  const [formData, setFormData] = useState();
+  const [errors, setErrors] = useState({});
+  const [mode, setMode] = useState("add");
+  const [showForm, setShowForm] = useState(false);
+
+  const [brand, setBrand] = useState({brand_id: ""});
+  const [brands, setBrands] = useState([]);
+  const [category, setCategory] = useState({category_id: ""});
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [pageSize, setPageSize] = useState(5);
+  const [pageNum, setPageNum] = useState(1);
+  const [search, setSearch] = useState("");
+
+  const [displayType, setDisplayType] = useState("table");
+
+  setFormData({
+    barcode: "",
+    name: "",
+    unit_cost_price: "",
+    unit_selling_price: "",
+    category_id: "",
+    brand_id: "",
+    brand_name: "",
+    created_at: "",
+    last_updated: "",
+  })
+
+  const fetchAllProducts = async () => {
+    const allProducts = await fetchAllProductsApi(pageSize, pageNum, search);
+    setProducts(Array.isArray(allProducts) ? allProducts : []);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      fetchAllProducts();
+    }, 500);
+    return () => clearTimeout(timeout);
+  }, [pageSize, pageNum, search]
+  );
+
+  const resetForm = () => {
+    setFormData({
+      barcode: "",
+      name: "",
+      unit_cost_price: "",
+      unit_selling_price: "",
+      category_id: "",
+      brand_id: "",
+      brand_name: "",
+      created_at: "",
+      last_updated: "",
+    });
+    setErrors({});
+    setMode("add");
+  };
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData((prev) => (
+      {
+        ...prev,
+        [name]: value,
+        ...(name === "brand_id" && value !== "" ? { brand_name: "" } : {})
+      }))
+  };
+
+  // handle submit for both add and update product events
+  // validates formData, makes post or put request and resets form
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await productValidationSchema.validate(formData, {abortEarly: false})
+
+      if (mode === "add") {
+        const productResponse = await addProductApi(formData);
+        productResponse && showToast(`${formData.name} added successfully`, "success");
+      }
+      else if (mode === "edit") {
+        const productResponse = await updateProductApi(formData, formData.id);
+        productResponse && showToast(`${formData.name} added successfully`, "success");
+      }
+      fetchAllProducts();
+      resetForm();
+    }
+    catch (error) {
+      console.error("Error: ", error)
+      if (error.inner) {
+        const newError = {}
+        for (const err of error.inner)
+          newError[err.path] = err.message
+        setErrors(newError);
+      }
+    }
+  };
+
+  const handleViewDetails = (product) => {setSelectedProduct(product);};
+
+  const handleEdit = async (product) => {
+    setMode("edit");
+    setShowForm(true);
+
+    const fresh_product = await fetchProductApi(product.id);
+    setFormData(fresh_product);
+  };
+
+  const handleDelete = async (product) => {
+    if (!window.confirm(`Are you sure you want to delete ${product.name}?`))
+      return;
+    await deleteProductApi();
+    fetchAllProducts();
+  };
+
+  const handleFetchBrands = async () => {
+    const allBrands = await fetchAllBrandsApi(null, null);
+    setBrands(Array.isArray(allBrands) ? allBrands : []);
+  }
+
+  const handleFetchCategories = async () => {
+    const allCategories = await fetchAllCategoriesApi(null, null);
+    setCategories(Array.isArray(allCategories) ? allCategories : []);
+  }
+
+  const handleFetchFilteredProducts = (e, type) => {
+    const {name, value} = e.target
+    
+    if (type === "brand")
+      setBrand({[name]: value})
+    else if (type === "category")
+      setCategory({[name]: value})
+  }
+
+  useEffect(() => {
+      async function fetchData () {
+        const filteredProducts = await fetchFilteredProductsByBrandAndCategory(
+          brand.brand_id, category.category_id, pageSize, pageNum
+        );
+        setProducts(filteredProducts ?? [])
+      }
+      fetchData();
+    }, [brand.brand_id, category.category_id, pageSize, pageNum])
+
+
+  return ({
+    formData, setFormData,
+    errors, setErrors,
+    mode, setMode,
+    showForm, setShowForm,
+    brand, setBrand,
+    brands, setBrands,
+    category, setCategory,
+    categories, setCategories,
+    products, setProducts,
+    selectedProduct, setSelectedProduct,
+    pageSize, setPageSize,
+    pageNum, setPageNum,
+    search, setSearch,
+    displayType, setDisplayType,
+    resetForm, handleChange, handleSubmit,
+    handleViewDetails, handleEdit, handleDelete,
+    handleFetchBrands, handleFetchCategories,
+    handleFetchFilteredProducts,
+  });
+}
