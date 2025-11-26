09af66a38dc16ffa64e24d38e0c3ad3c868e70d8
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-26 08:36:49 +0100

    feat: add products route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,18 @@ import BrandPage from "./pages/admin/BrandPage.jsx";
 import CategoryPage from "./pages/admin/CategoryPage.jsx";
 import LoginPage from "./pages/auth/login.jsx";
 import ProfilePage from "./pages/employees/profile.jsx";
+import ProductPage from "./pages/admin/productsPage.jsx";
 import RegistrationPage from "./pages/auth/register.jsx";
-// import ProductsPage from "./pages/admin/Products.jsx";
 
 
 function App() {
   return(
     <Routes>
-      {/* <Route path="/products" element={<ProductsPage />} /> */}
       <Route path="/brands" element={<BrandPage />} />
       <Route path="/categories" element={<CategoryPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/products" element={<ProductPage />} />
       <Route path="/register" element={<RegistrationPage />} />
       <Route path="/" element={<RegistrationPage />} />
     </Routes>
