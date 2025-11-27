4ee89ec731f896f08996a85f52ce9e9222b2bc99
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-27 09:59:32 +0100

    add user authentication  from AuthContext

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import "./Layout.css"
+import { useAuth } from "../hook/useAuth";
+import "./styles/Layout.css"
 
-function Layout({ main }){
+function Layout({main}){
+  const { employee, logout } = useAuth();
+  const [displayMenu, setDisplayMenu] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!employee)
+    navigate("/login");
+  }, [employee]);
+
     return(
         <>
-            <Header />
+            <Header
+              employee={employee}
+              logout={logout}
+              displayMenu={displayMenu}
+              setDisplayMenu={setDisplayMenu}
+            />
             
-            <div className="body-container">
-                <Sidebar />
+            <div className="main-container">
+                {displayMenu &&
+                <Sidebar employee={employee} />
+                }
                 {main}
             </div>
         </>
