0e228a0ce9779fca6f9781a2f85ff36bd9c6963e
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-27 09:51:16 +0100

    refactor: replace axios for api requests and rethrow error

--- frontend/src/api/categories.js
+++ frontend/src/api/categories.js
@@ -1,44 +1,40 @@
-import axios from "axios";
-
+import api from "./api";
 import { showToast } from "../utils/toast";
 
 
 export async function addCategoryApi(categoryData) {
   try{
-    let response = await axios.post("/api/v1/categories", categoryData);
+    let response = await api.post("/categories", categoryData);
     return response.data
   }
   catch (error) {
     if (error.response)
       showToast(error?.response?.data?.error || "Error adding category", "error");
-    else
-      console.error("error: ", error);
+    else {
+      console.error(error);
+      throw error;
+    }
   }
 }
 
 export async function updateCategoryApi(categoryData, category_id) {
   try{
-    let response = await axios.put(
-      `/api/v1/categories/${category_id}`,
-      categoryData,
-      {withCredentials: true}
-    );
+    let response = await api.put(`/categories/${category_id}`, categoryData);
     return response.data
   }
   catch (error) {
     if (error.response)
       showToast(error?.response?.data?.error || "Error updating category", "error");
-    else
-      console.error("error: ", error);
+    else {
+      console.error(error);
+      throw error;
+    }
   }
 }
 
 export async function fetchCategoryApi(category_id) {
   try{
-    let response = await axios.get(
-      `api/v1/categories/${category_id}`,
-      {withCredentials: true}
-    )    
+    let response = await api.get(`/categories/${category_id}`);
     return response.data;
   }
   catch (error) {
@@ -47,8 +43,10 @@ export async function fetchCategoryApi(category_id) {
       error?.response?.data?.error ||
       "Error fetching category. Please contact admin.", "error"
     );}
-    else
-      console.error("error: ", error);
+    else {
+      console.error(error);
+      throw error;
+    }
   }
 }
 
@@ -57,29 +55,25 @@ export async function fetchAllCategoriesApi(pageSize, pageNum, search) {
     pageSize = pageSize ?? 5
     pageNum = pageNum ?? 1
 
-    let response = await axios.get(
-      `api/v1/categories/${pageSize}/${pageNum}`,
-      {
-        withCredentials: true,
-        params: {search: search || ""}
-      }
-  )
+    let response = await api.get(
+      `/categories/${pageSize}/${pageNum}`,
+      {params: {search: search || ""}}
+  );
     return response.data;
   }
   catch (error) {
     if (error.response)
       console.log("error in fetchAllCategoriesApi: ", error.response.data.error)
-    else
-      console.error("error: ", error);
+    else {
+      console.error(error);
+      throw error;
+    }
   }
 }
 
 export async function deleteCategoryApi(category_id) {
   try{
-    await axios.delete(
-      `api/v1/categories/${category_id}`,
-      {withCredentials: true}
-    )
+    await api.delete(`/categories/${category_id}`);
   }
   catch (error) {
     if (error.response) {
@@ -87,7 +81,9 @@ export async function deleteCategoryApi(category_id) {
       error?.response?.data?.error ||
       "Error deleting category. Please contact admin.", "error"
     );}
-    else
-      console.error("error: ", error); 
+    else {
+      console.error(error);
+      throw error;
+    }
   }
 }
