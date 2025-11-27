84f682e7c5b02a38058672aa6f8b6baafc7aa80b
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-27 09:32:30 +0100

    feat: add AuthProvider to manage authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
+import {AuthProvider } from "./pages/auth/AuthProvider.jsx";
 import BrandPage from "./pages/admin/BrandPage.jsx";
 import CategoryPage from "./pages/admin/CategoryPage.jsx";
 import LoginPage from "./pages/auth/login.jsx";
@@ -10,15 +11,18 @@ import RegistrationPage from "./pages/auth/register.jsx";
 
 function App() {
   return(
-    <Routes>
-      <Route path="/brands" element={<BrandPage />} />
-      <Route path="/categories" element={<CategoryPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/products" element={<ProductPage />} />
-      <Route path="/register" element={<RegistrationPage />} />
-      <Route path="/" element={<RegistrationPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/" element={<RegistrationPage />} />
+
+        <Route path="/brands" element={<BrandPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
