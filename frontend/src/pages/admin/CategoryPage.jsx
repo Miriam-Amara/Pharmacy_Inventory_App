44ea1a48102090ed21ce3b9512cda012bdd18905
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-27 10:05:47 +0100

    feat: add css to category page

--- frontend/src/pages/admin/CategoryPage.jsx
+++ frontend/src/pages/admin/CategoryPage.jsx
@@ -13,6 +13,7 @@ import {
   DisplayCategoryDetailsModal,
 } from "../../components/CategoryDisplay";
 
+import "../pages.css"
 
 
 function CategoryPage() {
@@ -30,57 +31,60 @@ function CategoryPage() {
   } = useCategoryLogic();
   
   return (
-    <Layout main={
-    <main>
-      
-      {/* Title Section */}
-      <section>
-        <h3>Categories</h3>
-        <p>Add, delete, edit and view categories.</p>
-      </section>
+    <Layout
+      main={
+      <main className="content-container">
+        
+        {/* Title Section */}
+        <section className="title-section">
+          <h5>Categories</h5>
+          <p>Add, delete, edit and view categories.</p>
+        </section>
 
-      {/* Control Section */}
-      <section>
-        <div>
-          <AddCategory setMode={setMode} setShowForm={setShowForm} />
-          {showForm && <DisplayCategoryFormModal 
-            formData={formData}
-            errors={errors}
-            mode={mode}
-            handleChange={handleChange}
-            handleSubmit={handleSubmit}
-            resetForm={resetForm}
-          />}
-        </div>
+        {/* Control Section */}
+        <section className="control-section">
+          <div className="add">
+            <AddCategory setMode={setMode} setShowForm={setShowForm} />
 
-        <div>
-          <Pagination pageSize={pageSize} setPageSize={setPageSize} />
-        </div>
+            {showForm && <DisplayCategoryFormModal 
+              formData={formData}
+              errors={errors}
+              mode={mode}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              resetForm={resetForm}
+            />}
+          </div>
 
-        <div>
-          <SearchCategory search={search} setSearch={setSearch} />
-        </div>
-      </section>
+          <div className="pagination">
+            <Pagination pageSize={pageSize} setPageSize={setPageSize} />
+          </div>
 
-      {/* Display Section */}
-      <section>
-        {
-          selectedCategory && <DisplayCategoryDetailsModal
-            category={selectedCategory}
-            setSelectedCategory={setSelectedCategory}
+          <div className="search">
+            <SearchCategory search={search} setSearch={setSearch} />
+          </div>
+        </section>
+
+
+        {/* Display Section */}
+        <section className="display-section">
+          {
+            selectedCategory && <DisplayCategoryDetailsModal
+              category={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+            />
+          }
+
+          {
+            categories.length === 0 ? <p>No category found.</p> : <DisplayCategoriesTable
+            categories={categories}
+            handleViewDetails={handleViewDetails}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
           />
-        }
-
-        {
-          categories.length === 0 ? <p>No category found.</p> : <DisplayCategoriesTable
-          categories={categories}
-          handleViewDetails={handleViewDetails}
-          handleEdit={handleEdit}
-          handleDelete={handleDelete}
-        />
-        }
-      </section>
-    </main>
+          }
+        </section>
+      </main>
     }/>
   );
 }
