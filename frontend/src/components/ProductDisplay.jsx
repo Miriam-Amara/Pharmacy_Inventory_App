023365ee088a9b45474a4c539a2f397b97e9197a
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-28 05:52:06 +0100

    feat: add css style for form modal, info modal and table

--- frontend/src/components/ProductDisplay.jsx
+++ frontend/src/components/ProductDisplay.jsx
@@ -1,6 +1,8 @@
 /* */
 
-import ProductPage from "../pages/admin/productsPage";
+import deleteIcon from '../assets/delete_icon.png';
+import editIcon from "../assets/edit_icon.png";
+import viewIcon1 from "../assets/view_icon1.png";
 
 
 export function DisplayProductFormModal({
@@ -10,153 +12,158 @@ export function DisplayProductFormModal({
 }) {
   
   return (
-    <div>
-
-      <button
-        onClick={() => {resetForm(); setShowForm(false);}}
-      >
-        Cancel
-      </button>
-
-      <div>
-        {mode == "add" ? "Add" : "Edit"} Product
-      </div>
-
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Barcode:</label>
-          <div>
-            <input
-              type="text"
-              name="barcode"
-              value={formData.barcode}
-              onChange={handleChange}
-            />
-            {errors.barcode && <p>{errors.barcode}</p>}
-          </div>
-        </div>
-
-        <div>
-          <label>Product Name:</label>
-          <div>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              placeholder="Enter product name"
-              onChange={handleChange}
-            />
-            {errors.name && <p>{errors.name}</p>}
-          </div>
-        </div>
-
-        <div>
-          <label>Unit Cost Price (₦):</label>
-          <div>
-            <input
-              type="text"
-              name="unit_cost_price"
-              value={formData.unit_cost_price}
-              placeholder="Enter unit cost price"
-              onChange={handleChange}
-            />
-            {errors.unit_cost_price && <p>{errors.unit_cost_price}</p>}
-          </div>
-        </div>
-
-        <div>
-          <label>Unit Selling Price (₦):</label>
-          <div>
-            <input
-              type="text"
-              name="unit_selling_price"
-              value={formData.unit_selling_price}
-              placeholder="Enter unit selling price"
-              onChange={handleChange}
-            />
-            {errors.unit_selling_price && <p>{errors.unit_selling_price}</p>}
-          </div>
-        </div>
-
-        <div>
-          <label>Brand:</label>
-          <div>
-            <select
-              name="brand_id"
-              value={formData.brand_id}
-              onClick={() => {handleFetchBrands();}}
-              onChange={handleChange}
-            >
-              <option value="" disabled hidden>Select brand</option>
-              {brands.map((b) => (
-                <option key={b.id} value={b.id}>
-                  {b.name}
-                </option>
-              ))}
-              <option value="other">other:</option>
-            </select>
-            {errors.brand_id && <p>{errors.brand_id}</p>}
-          </div>
-
-          {formData.brand_id === "other" && (
-          <div>
-            <input
-              type="text"
-              name="brand_name"
-              placeholder="Enter brand name"
-              value={formData.brand_name || ""}
-              onChange={handleChange}
-              required
-            />
-            {errors.brand_name && <p>{errors.brand_name}</p>}
-          </div>
-        )}
-        </div>
-        
-        <div>
-          <label>Category:</label>
-          <div>
-            <select
-              name="category_id"
-              value={formData.category_id}
-              onClick={() => {handleFetchCategories();}}
-              onChange={handleChange}
-            >
-              <option value="" disabled hidden>Select category</option>
-              {categories.map((c) => (
-                <option key={c.id} value={c.id}>
-                  {c.name}
-                </option>
-              ))}
-            </select>
-            {errors.category_id && <p>{errors.category_id}</p>}
-          </div>
-        </div>
-
-        <div>
-          <label>Created At:</label>
-          <input
-            type="datetime-local"
-            name="created_at"
-            value={formData.created_at}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div>
-          <label>Last Updated:</label>
-          <input
-            type="datetime-local"
-            name="last_updated"
-            value={formData.last_updated}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button type="submit">
-          {mode == "add" ? "Save" : "Update"}
+    <div className="modal-container">
+      <div className="modal">
+        <button
+          onClick={() => {resetForm(); setShowForm(false);}}
+        >
+          Cancel
         </button>
-      </form>
+
+        <div>
+          {mode == "add" ? "Add" : "Edit"} Product
+        </div>
+
+        <form onSubmit={handleSubmit} className="form">
+          <div className="form-item">
+            <label>Barcode:</label>
+            <div>
+              <input
+                type="text"
+                name="barcode"
+                value={formData.barcode}
+                onChange={handleChange}
+              />
+              {errors.barcode && <p>{errors.barcode}</p>}
+            </div>
+          </div>
+
+          <div className="form-item">
+            <label>Product Name:</label>
+            <div>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                placeholder="Enter product name"
+                onChange={handleChange}
+              />
+              {errors.name && <p>{errors.name}</p>}
+            </div>
+          </div>
+
+          <div className="form-item">
+            <label>Unit Cost Price (₦):</label>
+            <div>
+              <input
+                type="text"
+                name="unit_cost_price"
+                value={formData.unit_cost_price}
+                placeholder="Enter unit cost price"
+                onChange={handleChange}
+              />
+              {errors.unit_cost_price && <p>{errors.unit_cost_price}</p>}
+            </div>
+          </div>
+
+          <div className="form-item">
+            <label>Unit Selling Price (₦):</label>
+            <div>
+              <input
+                type="text"
+                name="unit_selling_price"
+                value={formData.unit_selling_price}
+                placeholder="Enter unit selling price"
+                onChange={handleChange}
+              />
+              {errors.unit_selling_price && <p>{errors.unit_selling_price}</p>}
+            </div>
+          </div>
+
+          <div className="form-item">
+            <label>Brand:</label>
+            <div>
+              <select
+                name="brand_id"
+                value={formData.brand_id}
+                onClick={() => {handleFetchBrands();}}
+                onChange={handleChange}
+              >
+                <option value="" disabled hidden>Select brand</option>
+                {brands.map((b) => (
+                  <option key={b.id} value={b.id}>
+                    {b.name}
+                  </option>
+                ))}
+                <option value="other">other:</option>
+              </select>
+              {errors.brand_id && <p>{errors.brand_id}</p>}
+            </div>
+
+            {formData.brand_id === "other" && (
+            <div>
+              <input
+                type="text"
+                name="brand_name"
+                placeholder="Enter brand name"
+                value={formData.brand_name || ""}
+                onChange={handleChange}
+                required
+              />
+              {errors.brand_name && <p>{errors.brand_name}</p>}
+            </div>
+          )}
+          </div>
+          
+          <div className="form-item">
+            <label>Category:</label>
+            <div>
+              <select
+                name="category_id"
+                value={formData.category_id}
+                onClick={() => {handleFetchCategories();}}
+                onChange={handleChange}
+              >
+                <option value="" disabled hidden>Select category</option>
+                {categories.map((c) => (
+                  <option key={c.id} value={c.id}>
+                    {c.name}
+                  </option>
+                ))}
+              </select>
+              {errors.category_id && <p>{errors.category_id}</p>}
+            </div>
+          </div>
+
+          <div className="form-item">
+            <label>Created At:</label>
+            <div>
+              <input
+              type="datetime-local"
+              name="created_at"
+              value={formData.created_at}
+              onChange={handleChange}
+            />
+            </div>
+          </div>
+
+          <div className="form-item">
+            <label>Last Updated:</label>
+            <div>
+              <input
+              type="datetime-local"
+              name="last_updated"
+              value={formData.last_updated}
+              onChange={handleChange}
+            />
+            </div>
+          </div>
+
+          <button type="submit">
+            {mode == "add" ? "Save" : "Update"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
@@ -166,7 +173,7 @@ export function DisplayProductsTable({
   products, handleViewDetails, handleEdit, handleDelete
 }) {
   return (
-    <table>
+    <table className="content-table">
       <thead>
         <tr>
           <th>Product Name</th>
@@ -187,26 +194,29 @@ export function DisplayProductsTable({
             <td>{product.quantity_in_stock}</td>
             <td>{product.unit_cost_price}</td>
             <td>{product.unit_selling_price}</td>
-            <td>
+            <td className='table-actions'>
               <button
                 type="button"
                 onClick={() => handleViewDetails(product)}
+                className="btn btn-info-icon"
               >
-                Details
+                <img src={viewIcon1} alt="Info" />
               </button>
 
               <button
                 type="button"
                 onClick={() => handleEdit(product)}
+                className="btn btn-edit-icon"
               >
-                Edit
+                <img src={editIcon} alt="Edit" />
               </button>
 
               <button
                 type="button"
                 onClick={() => handleDelete(product)}
+                className="btn btn-danger-icon"
               >
-                Delete
+                <img src={deleteIcon} alt="Delete" />
               </button>
             </td>
           </tr>
@@ -219,7 +229,7 @@ export function DisplayProductsTable({
 
 export function DisplayProductsReorderingPoint({products}) {
   return (
-    <table>
+    <table className="content-table">
       <thead>
         <tr>
           <th>Product Name</th>
@@ -250,26 +260,36 @@ export function DisplayProductsGrid() {
 
 export function DisplayProductDetailsModal({product, setSelectedProduct}) {
   return(
-    <div>
-      <button
-        type="button"
-        onClick={() => {setSelectedProduct(null)}}
-      >
-        Close
-      </button>
-      <h4>Product Details</h4>
-      <p>ID: {product.id}</p>
-      <p>Date Created: {new Date(product.created_at).toLocaleString()}</p>
-      <p>Last Updated: {new Date(product.last_updated).toLocaleString()}</p>
-      <p>Added By: {product.added_by}</p>
-      <p>Product Name: {product.name}</p>
-      <p>Category: {product.category_name}</p>
-      <p>Brand: {product.brand_name}</p>
-      <p>Quantity In Stock: {product.quantity_in_stock}</p>
-      <p>Reorder: {product.is_below_reorder}</p>
-      <p>Reorder Quantity: {product.economic_ordering_quantity}</p>
-      <p>Unit Cost Price: {product.unit_cost_price}</p>
-      <p>Unit Selling Price: {product.unit_selling_price}</p>
+    <div className="details-modal-container">
+      <div className="details-modal">
+        <button
+          type="button"
+          onClick={() => {setSelectedProduct(null)}}
+        >
+          Close
+        </button>
+
+        <h5>Product Details</h5>
+
+        <div className="details">
+          <div className="details-title">
+            <p><span>ID:</span> {product.id}</p>
+            <p><span>Date Created:</span> {new Date(product.created_at).toLocaleString()}</p>
+            <p><span>Last Updated:</span> {new Date(product.last_updated).toLocaleString()}</p>
+            <p><span>Added By:</span> {product.added_by}</p>
+          </div>
+          <div className="details-content">
+            <p><span>Product Name:</span> {product.name}</p>
+            <p><span>Category:</span> {product.category_name}</p>
+            <p><span>Brand:</span> {product.brand_name}</p>
+            <p><span>Quantity In Stock:</span> {product.quantity_in_stock}</p>
+            <p><span>ReorderPoint:</span> {product.is_below_reorder}</p>
+            <p><span>Reorder Quantity:</span> {product.economic_ordering_quantity}</p>
+            <p><span>Unit Cost Price:</span> ₦ {product.unit_cost_price}</p>
+            <p><span>Unit Selling Price:</span> ₦ {product.unit_selling_price}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
