994996ea35c8cab85081037c638809f37be100e4
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-28 05:45:32 +0100

    feat: add css style for form modal, info modal and table displays

--- frontend/src/components/CategoryDisplay.jsx
+++ frontend/src/components/CategoryDisplay.jsx
@@ -1,5 +1,8 @@
 /* */
 
+import deleteIcon from '../assets/delete_icon.png';
+import editIcon from "../assets/edit_icon.png";
+import viewIcon1 from "../assets/view_icon1.png";
 
 export function DisplayCategoryFormModal(props) {
   const {
@@ -9,47 +12,48 @@ export function DisplayCategoryFormModal(props) {
   } = props;
 
   return (
-    <div>
+    <div className="modal-container">
+      <div className="modal">
+        <button onClick={() => {resetForm();}}>Cancel</button>
 
-      <button onClick={() => {resetForm();}}>Cancel</button>
+        <div>
+          {mode == "add" ? "Add" : "Edit"} Category
+        </div>
 
-      <div>
-        {mode == "add" ? "Add" : "Edit"} category
+        <form onSubmit={handleSubmit} className="form">
+          <div className="form-item">
+            <label>Category Name:</label>
+            <div>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                placeholder="Enter category name"
+                onChange={handleChange}
+              />
+              {errors.name && <p>{errors.name}</p>}
+            </div>
+          </div>
+
+          <div className="form-item">
+            <label>Description:</label>
+            <div>
+              <textarea
+                name="description"
+                value={formData.description}
+                placeholder="Enter category description"
+                onChange={handleChange}
+
+              />
+              {errors.description && <p>{errors.description}</p>}
+            </div>
+          </div>
+
+          <button type="submit">
+            {mode == "add" ? "Save" : "Update"}
+          </button>
+        </form>
       </div>
-
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Category Name:</label>
-          <div>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              placeholder="Enter category name"
-              onChange={handleChange}
-            />
-            {errors.name && <p>{errors.name}</p>}
-          </div>
-        </div>
-
-        <div>
-          <label>Description:</label>
-          <div>
-            <input
-              type="text"
-              name="description"
-              value={formData.description}
-              placeholder="Enter category description"
-              onChange={handleChange}
-            />
-            {errors.description && <p>{errors.description}</p>}
-          </div>
-        </div>
-
-        <button type="submit">
-          {mode == "add" ? "Save" : "Update"}
-        </button>
-      </form>
     </div>
   );
 }
@@ -58,11 +62,10 @@ export function DisplayCategoryFormModal(props) {
 export function DisplayCategoriesTable(props) {
   const {categories, handleViewDetails, handleEdit, handleDelete} = props
   return (
-    <table>
+    <table className="content-table">
       <thead>
         <tr>
           <th>Category Name</th>
-          <th>Description</th>
           <th>Date Created</th>
           <th>Actions</th>
         </tr>
@@ -71,28 +74,30 @@ export function DisplayCategoriesTable(props) {
         {categories.map((category) => (
           <tr key={category.id}>
             <td>{category.name}</td>
-            <td>{category.description}</td>
             <td>{new Date(category.created_at).toLocaleDateString()}</td>
-            <td>
+            <td className='table-actions'>
               <button
                 type="button"
                 onClick={() => handleViewDetails(category)}
+                className="btn btn-info-icon"
               >
-                Details
+                <img src={viewIcon1} alt="Info" />
               </button>
 
               <button
                 type="button"
                 onClick={() => handleEdit(category)}
+                className="btn btn-edit-icon"
               >
-                Edit
+                <img src={editIcon} alt="Edit" />
               </button>
 
               <button
                 type="button"
                 onClick={() => handleDelete(category)}
+                className="btn btn-danger-icon"
               >
-                Delete
+                <img src={deleteIcon} alt="Delete" />
               </button>
             </td>
           </tr>
@@ -105,20 +110,31 @@ export function DisplayCategoriesTable(props) {
 
 export function DisplayCategoryDetailsModal({category, setSelectedCategory}) {
   return(
-    <div>
-      <button
-        type="button"
-        onClick={() => {setSelectedCategory(null)}}
-      >
-        Close
-      </button>
-      <h4>Category Details</h4>
-      <p>ID: {category.id}</p>
-      <p>Date Created: {new Date(category.created_at).toLocaleString()}</p>
-      <p>Last Updated: {new Date(category.last_updated).toLocaleString()}</p>
-      <p>Added By: {category.added_by}</p>
-      <p>Category Name: {category.name}</p>
-      <p>Description: {category.description}</p>
+    <div className="details-modal-container">
+      <div className="details-modal">
+        <button
+          type="button"
+          onClick={() => {setSelectedCategory(null)}}
+        >
+          Close
+        </button>
+
+        <h5>Category Details</h5>
+
+        <div className="details">
+          <div className="details-title">
+            <p><span>ID:</span> {category.id}</p>
+            <p><span>Date Created:</span> {new Date(category.created_at).toLocaleString()}</p>
+            <p><span>Last Updated:</span> {new Date(category.last_updated).toLocaleString()}</p>
+            <p><span>Added By:</span> {category.added_by}</p>
+          </div>
+
+          <div className="details-content">
+            <p><span>Category Name:</span> {category.name}</p>
+            <p><span>Description:</span> {category.description}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
