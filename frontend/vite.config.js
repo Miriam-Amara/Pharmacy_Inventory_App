1109a1157739ee26c14824a91965ff3f4bb42267
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-11-29 18:42:01 +0100

    refactor: remove server proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target:"https://pharmacy-inventory-app.onrender.com",
-        changeOrigin: true,
-        secure: true,
-      }
-    }
-  }
-})
+export default defineConfig({plugins: [react()]})
